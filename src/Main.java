50829c18f26a21f55c7be639b5ac0e3926380cf2
Ghost116rus <danilkaleev@mai.ru>
2023-02-05 18:52:44 +0300

    Оказывается изначально сделал 4-ый вариант

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,26 @@
 public class Main {
     public static void main(String[] args)
     {
-        int positive_sum = 0;
-        int negative_sum = 0;
+        int sum1 = 0;
+        int sum2 = 0;
 
-        for (String x : args) {//Вывод всех заданных параметров командной строки
-
+        for (String x : args)
+        {//Вывод всех заданных параметров командной строки
         /* преобразвание строки, хранящейся в переменной "х" в целое число
         и проверка этого числа на чётность*/
             int number = Integer.parseInt(x);
-            if ((number % 2 == 0) && (number >= 0)) {
-                positive_sum += number;
-            }
-            if (((number * -1) % 2 == 1) && (number < 0)) {
-                negative_sum += number;
+            if (number >= 0)
+            {
+                if (number % 2 == 1){
+                    sum1 += number;
+                }
+                else {
+                    sum2 += number;
+                }
             }
         }
-
-        System.out.println(positive_sum);//вывод "нечётного" списка
-        System.out.println(negative_sum);//вывод "чётного" списка
+        System.out.println(sum1);//вывод "нечётного" списка
+        System.out.println(sum2);//вывод "чётного" списка
 
     }
 }
